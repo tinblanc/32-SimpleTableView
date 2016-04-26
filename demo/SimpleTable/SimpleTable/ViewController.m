0fc8fba1bf7c1285180011ee34dd8bcac6a67774
tinblanc <tin.zz123@gmail.com>
2016-04-26 15:53:40 +0700

    demo simple tableview

---
+++ demo/SimpleTable/SimpleTable/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  SimpleTable
+//
+//  Created by Tin Blanc on 4/26/16.
+//  Copyright © 2016 Tin Blanc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
